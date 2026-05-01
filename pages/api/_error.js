227f4d0b9f5561d07867990c8c3eb676da0a8c91
logwickio <jesse@logwick.io>
2026-05-01 14:53:02 -0400

    Add catch-all JSON 404 handler for API routes

--- pages/api/_error.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json')
-  res.status(500).json({
-    error: 'Internal server error',
-    code: 500,
-    message: 'An unexpected error occurred',
-    docs: 'https://logwick.io/docs'
-  })
-}
